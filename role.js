caf293084b0191b8ff8a0a9081dc2b44826dc132
K4y31 <39564396+K4y31@users.noreply.github.com>
2020-07-30 16:10:04 -0500

    Update role.js

--- role.js
+++ role.js
@@ -1,187 +1,223 @@
-// Import constructors, configuration and login the client
-const { Client, MessageEmbed, Emoji, MessageReaction } = require('discord.js');
-const CONFIG = require('./config');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = "NzM1OTk3ODI1MDgyNzIwMjg5.XxobaA.bLq8oiUoGM-FG8FfsW7FeKG9Ebo";
+const PREFIX = "!";
 
-const client = new Client({ disableEveryone: true });
-if (CONFIG.botToken === '')
-    throw new Error("The 'botToken' property is not set in the config.js file. Please do this!");
+// Initialize
+client.on('ready', () => {
+    console.log("Maid Mint is here!");
+    //client.user.setActivity('Niji no Conquistador', {type: 'WATCHING'}); //ver 12.2.0
+    client.user.setPresence({
+        game: {
+            name: 'Niji no Conquistador',
+            type: "Watching"
+        }
+    }); //ver 11.1.0
+    //client.user.setGame('Niji no Conquistador');
+});
 
-client.login(CONFIG.botToken);
+// Greets member
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if(!channel) return;
+    channel.send(`お帰りなさいませ、${member}ご主人様！, please read the rules and follow them!`);
+    
+    /*var role = member.guild.roles.find(role => role.name == "Fandôme");
+    member.addRole(role);*/
+});
 
-// If there isn't a reaction for every role, alert the user
-if (CONFIG.roles.length !== CONFIG.reactions.length)
-    throw "Roles list and reactions list are not the same length! Please double check this in the config.js file";
+// Add role on reaction
+client.on('messageReactionAdd', (reaction, user) => {
+  if (message.channel.id === '738484460227395595') {
+    if (reaction.emoji.name === "kimoi") {
+      const guildMember = reaction.message.guild.members.get(user.id);
+      const role = reaction.message.guild.roles.get(role => role.name == "Fandôme");
+      guildMember.addRole(role);
+    }
+  }
+});
 
-// Function to generate the role messages, based on your settings
-function generateMessages() {
-    return CONFIG.roles.map((r, e) => {
-        return {
-            role: r,
-            message: `React below to get the **"${r}"** role!`, //DONT CHANGE THIS,
-            emoji: CONFIG.reactions[e]
-        };
-    });
-}
+// Functions
 
-// Function to generate the embed fields, based on your settings and if you set "const embed = true;"
-function generateEmbedFields() {
-    return CONFIG.roles.map((r, e) => {
-        return {
-            emoji: CONFIG.reactions[e],
-            role: r
-        };
-    });
-}
+client.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    const filter = (reaction, user) => ['mintsmug'].includes(reaction.emoji.name) && user.id === message.author.id;
+                                                        // <<<<<<<<<< MODERATION >>>>>>>>>>>
+    // !kick <user> <reason>
+    if (cmd === `${PREFIX}kick`) {   
+        // Check Role
+        if (!message.member.roles.find(role => role.name === 'Moderators')) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
+        
+        if (args[0] == null) {
+            message.channel.send('You need to specify a user for this kick. \n\`Usage: ' + PREFIX + 'kick <user> <reason>\`');
+            return;
+        } else if (args[1] == null) {
+            message.channel.send('You need to specify a reason for this kick. \n\`Usage: ' + PREFIX + 'kick <user> <reason>\`');
+            return;
+        }
+        
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+        if (!kUser) message.channel.send('That user is not in this server.');
+        let kReason = args.join(" ").slice(22);
+        
+        if (kUser.roles.find(role => role.name === 'Moderators')) return message.channel.send("That user can't be kicked.");
 
-// Client events to let you know if the bot is online and to handle any Discord.js errors
-client.on("ready", () => console.log("Role Reactions is online!"));
-client.on('error', console.error);
+        let kickEmbed = new Discord.RichEmbed().setDescription('~Kick~').setColor('#fff203').addField('Kicked User', `${kUser} with ID ${kUser.id}`).addField('Kicked By ', `${message.author} > with ID ${message.author.id}`).addField('Time ', message.createdAt).addField('Reason', kReason);
 
-// Handles the creation of the role reactions. Will either send the role messages separately or in an embed
-client.on("message", message => {
-    // Make sure bots can't run this command
-    if (message.author.bot) return;
+        let reportChannel = message.guild.channels.find(channel => channel.name === 'report');
+        if (!reportChannel) return message.channel.send("Can't find #report channel.");
 
-    // Make sure the command can only be ran in a server
-    if (!message.guild) return;
-
-    // We don't want the bot to do anything further if it can't send messages in the channel
-    if (message.guild && !message.channel.permissionsFor(message.guild.me).missing('SEND_MESSAGES')) return;
-
-    if ((message.author.id !== CONFIG.yourID) || (message.content.toLowerCase() !== CONFIG.setupCMD)) return;
-
-    if (CONFIG.deleteSetupCMD) {
-        const missing = message.channel.permissionsFor(message.guild.me).missing('MANAGE_MESSAGES');
-        // Here we check if the bot can actually delete messages in the channel the command is being ran in
-        if (missing.includes('MANAGE_MESSAGES'))
-            throw new Error("I need permission to delete your command message! Please assign the 'Manage Messages' permission to me in this channel!");
-        message.delete().catch(O_o=>{});
+        message.guild.member(kUser).kick(kReason);
+        message.channel.send(`Successfully kicked ${kUser}.`);
+        reportChannel.send(kickEmbed);
     }
 
-    const missing = message.channel.permissionsFor(message.guild.me).missing('MANAGE_MESSAGES');
-    // Here we check if the bot can actually add recations in the channel the command is being ran in
-    if (missing.includes('ADD_REACTIONS'))
-        throw new Error("I need permission to add reactions to these messages! Please assign the 'Add Reactions' permission to me in this channel!");
-
-    if (!CONFIG.embed) {
-        if (!CONFIG.initialMessage || (CONFIG.initialMessage === '')) 
-            throw "The 'initialMessage' property is not set in the config.js file. Please do this!";
-
-        message.channel.send(CONFIG.initialMessage);
-
-        const messages = generateMessages();
-        for (const { role, message: msg, emoji } of messages) {
-            if (!message.guild.roles.find(r => r.name === role))
-                throw `The role '${role}' does not exist!`;
-
-            message.channel.send(msg).then(async m => {
-                const customCheck = message.guild.emojis.find(e => e.name === emoji);
-                if (!customCheck) await m.react(emoji);
-                else await m.react(customCheck.id);
-            }).catch(console.error);
-        }
-    } else {
-        if (!CONFIG.embedMessage || (CONFIG.embedMessage === ''))
-            throw "The 'embedMessage' property is not set in the config.js file. Please do this!";
-        if (!CONFIG.embedFooter || (CONFIG.embedMessage === ''))
-            throw "The 'embedFooter' property is not set in the config.js file. Please do this!";
-
-        const roleEmbed = new MessageEmbed()
-            .setDescription(CONFIG.embedMessage)
-            .setFooter(CONFIG.embedFooter);
-
-        if (CONFIG.embedColor) roleEmbed.setColor(CONFIG.embedColor);
-
-        if (CONFIG.embedThumbnail && (CONFIG.embedThumbnailLink !== '')) 
-            roleEmbed.setThumbnail(CONFIG.embedThumbnailLink);
-        else if (CONFIG.embedThumbnail && message.guild.icon)
-            roleEmbed.setThumbnail(message.guild.iconURL);
-
-        const fields = generateEmbedFields();
-        if (fields.length > 25) throw "That maximum roles that can be set for an embed is 25!";
-
-        for (const { emoji, role } of fields) {
-            if (!message.guild.roles.cache.find(r => r.name === role))
-                throw `The role '${role}' does not exist!`;
-
-            const customEmote = client.emojis.cache.find(e => e.name === emoji);
-            
-            if (!customEmote) roleEmbed.addField(emoji, role, true);
-            else roleEmbed.addField(customEmote, role, true);
+    // !ban <user> <reason>
+    if (cmd === `${PREFIX}ban`) {   
+        // Check Role
+        if (!message.member.roles.find(role => role.name === 'Moderators')) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
         }
 
-        message.channel.send(roleEmbed).then(async m => {
-            for (const r of CONFIG.reactions) {
-                const emoji = r;
-                const customCheck = client.emojis.cache.find(e => e.name === emoji);
-                
-                if (!customCheck) await m.react(emoji);
-                else await m.react(customCheck.id);
+        if (args[0] == null) {
+            message.channel.send('You need to specify a user for this ban. \n\`Usage: ' + PREFIX + 'ban <user> <reason>\`');
+            return;
+        } else if (args[1] == null) {
+            message.channel.send('You need to specify a reason for this ban. \n\`Usage: ' + PREFIX + 'ban <user> <reason>\`');
+            return;
+        }
+        let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!bUser) message.channel.send('That user is not in this server.');
+        let bReason = args.join(" ").slice(22);
+        
+        if (bUser.roles.find(role => role.name === 'Moderators')) return message.channel.send("That user can't be banned.");
+
+        let banEmbed = new Discord.RichEmbed().setDescription('~Ban~').setColor('#eb4034').addField('Banned User', `${bUser} with ID ${bUser.id}`).addField('Banned By ', `${message.author} > with ID ${message.author.id}`).addField('Time ', message.createdAt).addField('Reason', bReason);
+
+        let reportChannel = message.guild.channels.find(channel => channel.name === 'report');
+        if (!reportChannel) return message.channel.send("Can't find #report channel.");
+
+        message.guild.member(bUser).ban(bReason);
+        message.channel.sendMessage(`Successfully banned ${bUser}.`);
+        reportChannel.send(banEmbed);
+    }
+
+    // !unban <user> <reason>
+    /*if (cmd === `${PREFIX}unban`) {   
+        // Check Role
+        if (!message.member.roles.find(role => role.name === 'Moderators')) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
+
+        if (messageArray[1] == null) {
+            message.channel.sendMessage('You need to specify a user for this unban. \n\`Usage: ' + PREFIX + 'unban <user> <reason>\`');
+            return;
+        } else if (messageArray[2] == null) {
+            message.channel.sendMessage('You need to specify a reason for this unban. \n\`Usage: ' + PREFIX + 'unban <user> <reason>\`');
+            return;
+        }
+        let uUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!uUser) message.channel.send('That user is not in this server.');
+        let uReason = args.join(" ").slice(22);
+
+        let unbanEmbed = new Discord.RichEmbed().setDescription('~Unban~').setColor('#66ffcc').addField('Unbanned User', `${uUser} > with ID ${uUser.id}`).addField('Unbanned By ', `${message.author} > with ID ${message.author.id}`).addField('Time ', message.createdAt).addField('Reason', uReason);
+
+        let reportChannel = message.guild.channels.find(channel => channel.name === 'report');
+        if (!reportChannel) return message.channel.send("Can't find #report channel.");
+
+        message.guild.member(uUser).unban(uReason);
+        message.channel.send(`Successfully unbanned ${uUser}.`);
+        reportChannel.send(unbanEmbed);
+    }*/
+
+                                                                // <<<<<<<<<< MISCELLANOUS >>>>>>>>>
+    // !say <text>
+    if (cmd === `${PREFIX}say`) {
+        // Check Role
+        if (!message.member.roles.find(role => role.name === 'Moderators')) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
+
+        if(args[0] == null) {
+            message.channel.send('Missing argument(s). \n\`Usage: ' + PREFIX + 'say <text>\`');
+            return;
+        } else {
+            message.delete();
+            message.channel.send(args.join(" "));
+        }
+    }
+
+    // !purge <amount>
+    if (cmd === `${PREFIX}purge`) {
+        async function purge() {
+            // Check Role
+            if (!message.member.roles.find(role => role.name === 'Moderators')) {
+                message.channel.send('You do not have permission to use this command.');
+                return;
             }
+
+            if (isNaN(args[0])) {
+                message.channel.send('You must specify a number as your argument. \n\`Usage: ' + PREFIX + 'purge <amount>\`');
+                return;
+            }
+            
+            if (args[0] < 2 || args[0] > 100) {
+                message.channel.send('You must specify a number between 2 and 100. \n\`Usage: ' + PREFIX + 'purge <amount>\`');  
+                return;
+            }
+
+            message.delete();
+            const fetched = await message.channel.fetchMessages({ limit: args[0] });
+            console.log(fetched.size + ' messages found, deleting...');
+            message.channel.bulkDelete(fetched).catch(error => message.channel.send(`Error: ${error}`));
+        }
+        
+        purge();
+    }
+    
+    // !getrolemessage
+    if (cmd === `${PREFIX}getrolemessage`){
+        // Check Role
+        if (!message.member.roles.find(role => role.name === 'Moderators')) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
+        
+        const fandome = message.guild.roles.get('738485578085040208');
+
+        message.channel.send('React to this message with :mintsmug: to get Fandôme role and access the rest of the server!').then(async msg => {
+            await msg.react('mintsmug'); 
+
+            msg.awaitReactions(filter, {
+                max: '1',
+                time: 30000,
+                errors: ['time']
+            }).then(collected => {
+                const reaction = collected.first();
+
+                switch (reaction.emoji.name){
+                    case 'mintsmug':
+                        message.member.addRole(fandome).catch(err => {
+                            console.log(err);
+                            return message.channel.send(`Error adding you to this role: ${err.message}.`)
+                        });
+                        message.channel.send(`You have been added to the **${fandome.name}**!`).then(m => m.delete(3000));
+                        msg.delete();
+                        break;
+                }
+            }).catch(collected =>{
+                return message.channel.send("Can't add you to this role.");
+            });
         });
     }
-});
+})
 
-// This makes the events used a bit more readable
-const events = {
-	MESSAGE_REACTION_ADD: 'messageReactionAdd',
-	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
-};
-
-// This event handles adding/removing users from the role(s) they chose based on message reactions
-client.on('raw', async event => {
-    if (!events.hasOwnProperty(event.t)) return;
-
-    const { d: data } = event;
-    const user = client.users.cache.get(data.user_id);
-    const channel = client.channels.cache.get(data.channel_id);
-
-    const message = await channel.messages.fetch(data.message_id);
-    const member = message.guild.members.cache.get(user.id);
-
-    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-    let reaction = message.reactions.cache.get(emojiKey);
-
-    if (!reaction) {
-        // Create an object that can be passed through the event like normal
-        const emoji = new Emoji(client.guilds.cache.get(data.guild_id), data.emoji);
-        reaction = new MessageReaction(message, emoji, 1, data.user_id === client.user.id);
-    }
-
-    let embedFooterText;
-    if (message.embeds[0]) embedFooterText = message.embeds[0].footer.text;
-
-    if (
-        (message.author.id === client.user.id) && (message.content !== CONFIG.initialMessage || 
-        (message.embeds[0] && (embedFooterText !== CONFIG.embedFooter)))
-    ) {
-
-        if (!CONFIG.embed && (message.embeds.length < 1)) {
-            const re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            const role = message.content.match(re)[1];
-
-            if (member.id !== client.user.id) {
-                const guildRole = message.guild.roles.cache.find(r => r.name === role);
-                if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(guildRole.id);
-                else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(guildRole.id);
-            }
-        } else if (CONFIG.embed && (message.embeds.length >= 1)) {
-            const fields = message.embeds[0].fields;
-
-            for (const { name, value } of fields) {
-                if (member.id !== client.user.id) {
-                    const guildRole = message.guild.roles.cache.find(r => r.name === value);
-                    if ((name === reaction.emoji.name) || (name === reaction.emoji.toString())) {
-                        if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(guildRole.id);
-                        else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(guildRole.id);
-                    }
-                }
-            }
-        }
-    }
-});
-
-process.on('unhandledRejection', err => {
-    const msg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
-	console.error("Unhandled Rejection", msg);
-});
+client.login(token);
